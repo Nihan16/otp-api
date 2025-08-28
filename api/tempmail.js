4a19b182567a45acaafe49deabdb78c2c78dcbd4
Nihan16 <152619151+Nihan16@users.noreply.github.com>
2025-08-28 14:46:12 +0600

    Update tempmail.js

--- api/tempmail.js
+++ api/tempmail.js
@@ -1,6 +1,14 @@
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
+  // CORS headers যোগ করুন
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   const { email } = req.query;
 
   if (!email) {
